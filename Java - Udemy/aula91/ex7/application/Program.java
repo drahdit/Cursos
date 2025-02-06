510c872b5a066b27555cbb31b2b7d93580b6c4a3
Richard <80552873+drahdit@users.noreply.github.com>
2025-02-06 14:45:48 -0300

    Update Program.java

--- Java - Udemy/aula91/ex7/application/Program.java
+++ Java - Udemy/aula91/ex7/application/Program.java
@@ -1,3 +1,9 @@
+/*
+Fazer um programa para ler um número inteiro N e depois um vetor de N números reais. Em seguida, 
+mostrar na tela a média aritmética de todos elementos com três casas decimais. Depois mostrar todos 
+os elementos do vetor que estejam abaixo da média, com uma casa decimal cada.
+*/
+
 package aula91.ex7.application;
 
 import java.util.Scanner;
